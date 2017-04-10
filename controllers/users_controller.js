de46aa8cfe147be8bb6fea1f9604ae9b9be8c322
Cara Chew <carachew@gmail.com>
2017-04-10 17:48:47 +0800

    RESTFUL routes with USER database completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,23 @@
 var express = require('express')
 var router = express.Router()
 
+
 //SET UP MOVIE model
 var User = require('../models/user')
+
+//ROUTE FOR FORMS
+router.get('/users/new', function(req, res) {
+  res.send('New User Created')
+})
+
+router.get('/users/:id/edit', function(req, res) {
+
+  User.findById(req.params.id, function (err, User) {
+    if (err) next()
+    res.render('users/edit', {User})
+  })
+})
+
 
 
 //GETS THE ENTIRE DATABASE
@@ -25,6 +40,29 @@ router.post('/user', function (req,res) {
   // newMovie.name = req.body.name
   // newMovie.synopsis = req.body.synopsis
   // newMovie.rating = req.body.rating
+})
+
+router.get('/users/:id', function(req, res) {
+  res.send('individual user details on id ' + req.params.id)
+})
+
+router.put('/users/:id', function(req, res, next) {
+
+  User.findByIdAndUpdate(req.params.id, req.body.user, function(err, updatedData) {
+    if (err) next()
+    res.redirect('/users/:id') //define a page
+  })
+})
+
+router.post('/users/:id', function(req, res) {
+  res.send('NOT HERE')
+})
+
+router.delete('/users/:id', function(req, res, next) {
+  User.findByIdAndRemove(req.params.id, function (err, found) {
+    if (err) throw (err)
+    res.send('user deleted')
+  })
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 4000
-
-
+var methodOverride = require('method-override')
 // mongoose setup
 var dbURI = process.env.PROD_MONGODB || 'mongodb://localhost:27017/mymdb'
 var mongoose = require('mongoose')
@@ -18,19 +17,26 @@ db.once('open', function() {
 
 // setup body parser
 var bodyParser = require('body-parser')
+
+//TRANSFORM FORM DATA TO REQ.BODY
+app.use(bodyParser.urlencoded({extended:true}))
+//TRANSFORM JSON TO REQ.BODY
 app.use(bodyParser.json())
 
 app.get('/', function(req,res) {
   res.send('homepage')
 })
 
+app.set('view engine', 'ejs')
+app.use(methodOverride('_method'))
+
 // require the movies_controller
 var usersController = require('./controllers/users_controller')
 app.use(usersController)
 
-app.use(function (req, res) {
-  res.send('error found')
-})
+// app.use(function (req, res) {
+//   res.send('error found')
+// })
 
 app.listen(port, function () {
   console.log('app is running at ' + port)
